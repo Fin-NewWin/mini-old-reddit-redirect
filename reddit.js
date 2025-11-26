f35afdffb7b89eed0bf11cbc073bfe384f629755
Phien Nguyen <nguyenphien17@yahoo.com>
2025-11-25 20:09:18 -0800

    init js

---
+++ reddit.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name               Old Reddit Redirect
+// @description        Redirect to old reddit, forked from RandomUsername404
+// @include            *://www.reddit.com/*
+// @exclude            *://www.reddit.com/poll/*
+// @exclude            *i.redd.it*
+// @version            1.00
+// @run-at             document-start
+// @author             Fin-NewWin
+// ==/UserScript==
+
+(function() {
+    if (window.location.pathname === "/media"){
+        window.location.replace(new URL(window.location.href).searchParams.get("url"))
+    } else {
+        window.location.replace("https://old.reddit.com" + window.location.pathname + window.location.search);
+    }
+})();
